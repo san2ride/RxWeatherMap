fbf3394897818bcba0829e5c8e35e91c290076e9
Jason Sanchez <jason@xao.live>
2019-07-11 14:19:36 -0600

    URLRequest Extension

---
+++ RxWeather/RxWeather/extensions/URLRequest+Extensions.swift
@@ -0,0 +1,30 @@
+//
+//  URLRequest+Extensions.swift
+//  RxWeather
+//
+//  Created by Jason Sanchez on 7/11/19.
+//  Copyright © 2019 Jason Sanchez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+struct Resource<T> {
+    let url: URL
+}
+
+extension URLRequest {
+    
+    static func load<T: Decodable>(resource: Resource<T>) -> Observable<T> {
+        
+        return Observable.from([resource.url])
+            .flatMap { url -> Observable<Data> in
+                let request = URLRequest(url: url)
+                return URLSession.shared.rx.data(request: request)
+            }.map { data -> T in
+                return try JSONDecoder().decode(T.self, from: data)
+        }.asObservable()
+    }
+}
